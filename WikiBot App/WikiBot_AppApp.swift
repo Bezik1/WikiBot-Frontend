673019a243048ae14ff9ad8714cc398e7eb852fc
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2025-08-14 22:18:48 +0200

    Initial Commit

---
+++ WikiBot App/WikiBot_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiBot_AppApp.swift
+//  WikiBot App
+//
+//  Created by MacBook Mateusz Adamowicz on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct WikiBot_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WikiBot AppTests/WikiBot_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WikiBot_AppTests.swift
+//  WikiBot AppTests
+//
+//  Created by MacBook Mateusz Adamowicz on 14/08/2025.
+//
+
+import Testing
+@testable import WikiBot_App
+
+struct WikiBot_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
